5a31157443bda72349e1a1ade47dc9f212546c1e
adetokefad <adetokefaderin@gmail.com>
2025-04-11 22:32:42 +0100

    siteplan

---
+++ project/scripts/siteplandates.js
@@ -0,0 +1,5 @@
+// Get the current year for copyright
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+// Get last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
